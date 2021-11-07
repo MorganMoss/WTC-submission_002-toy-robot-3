d16bee9531089ef78d765a51e72dabadc34cb5fb
mmoss <mmoss020@student.wethinkcode.co.za>
2021-11-07 13:34:50 +0200

    starting on test cmd hndler

--- test_command_handler.py
+++ test_command_handler.py
@@ -2,6 +2,7 @@ import unittest
 from io import StringIO
 from test_base import captured_io
 from robot_command_handling import CommandHandler
+from exceptions import InputError
 
 
 class TestCommandHandler(unittest.TestCase):
@@ -25,14 +26,17 @@ class TestCommandHandler(unittest.TestCase):
 
     def test_get_command(self):
         for key in ["LEFT", "RIGHT", "HELP", "REPLAY", "OFF"]:
-            # input_string = "Fail\n"
-            input_string = f"{key}\n"
+            input_string = "Fail\n"
+            input_string += f"{key}\n"
             input_string += f"{key.lower()}\n"
             input_string += f"{key[0]}{key[1:len(key)-1].lower()}{key[-1]}\n"
             input_string += f"{key[0].lower()}{key[1:len(key)-1]}{key[-1].lower()}\n"
 
             with captured_io(StringIO(input_string)) as (out, err):
-                # self.assertRaises(self.handler_robby.get_command(), InputError)
+                try:
+                    self.handler_robby.get_command()
+                except InputError as e:
+                    self.assertEqual("Sorry, I did not understand 'Fail'.", str(e))
                 for _ in range(4):
                     self.assertEquals(self.handler_robby.get_command()[0].upper(), key)
 
